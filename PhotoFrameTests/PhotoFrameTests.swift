953225cf8e039879822174891afa28f59351a7d6
SinanDaniel <sinan.cavusoglu@hisarschool.k12.tr>
2024-09-18 18:33:46 +0300

    Initial Commit

---
+++ PhotoFrameTests/PhotoFrameTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFrameTests.swift
+//  PhotoFrameTests
+//
+//  Created by Sinan Cavusoglu on 18.09.2024.
+//
+
+import Testing
+@testable import PhotoFrame
+
+struct PhotoFrameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
